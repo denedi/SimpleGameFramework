62e57085e820d44750ab2fd04c246ceb96fcd457
Deybi Diaz <denedi@dnedik.com>
2013-12-18 08:28:24 -0500

    template method to initialize the game

--- SimpleGame.java
+++ SimpleGame.java
@@ -4,20 +4,32 @@ public class SimpleGame implements Game, Runnable {
     private GController mainController;
 
     public SimpleGame() {
+        init();
+    }
+    
+    protected void init() {
         simpleWorld = createWorld();
         mainView = new SView( simpleWorld );
         mainController = createController();
     }
 
+    protected GWorld createWorld() {
+        return new SimpleWBDirector().construct(new SimpleWBuilder);
+    }
+
+    protected GView createView() {
+        return new SimpleGView(simpleWorld);
+    }
+
+    protected GController createController() {
+        return new SimpleGController(simpleWorld, mainView);
+    }
+    
     public void launch() {
         new Thread(this).start();
     }
 
     public void run() {
         simpleWorld.update();
-    }
-
-    protected GController createController() {
-        
     }
 }
